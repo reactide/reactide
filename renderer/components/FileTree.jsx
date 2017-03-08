b2738ac171521d3c22ad5c5e540ecf66e6ec9d53
Jin Choi <jinchoi@ucdavis.edu>
2017-03-07 20:47:06 -0800

    cleaned up some more

--- renderer/components/FileTree.jsx
+++ renderer/components/FileTree.jsx
@@ -12,18 +12,17 @@ export default class FileTree extends React.Component {
 
   fileTreeInit() {
     ipcRenderer.on('openDir', (event, dirPathArr) => {
-      //dirPath is an array with the absolute path to the opened directory
-      this.setFileTree(dirPathArr[0]);
-      let watch = fs.watch(dirPathArr[0], {recursive: true}, (eventType, fileName) => {
-        this.setFileTree(dirPathArr[0]);
+      this.setFileTree(dirPathArr);
+      let watch = fs.watch(dirPathArr, {recursive: true}, (eventType, fileName) => {
+        this.setFileTree(dirPathArr);
       });
     });
   }
 
   setFileTree(path) {
     const projFileTree = fileTree(path);
-    console.log(JSON.stringify(projFileTree, null, this.state.num));
   }
+
   render() {
     return (
       <div>
