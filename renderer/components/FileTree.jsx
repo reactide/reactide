3a998f883c07a7d71d5ec116fd3660185192f62a
Admin <admin@Admins-MacBook-Pro.local>
2017-03-10 15:37:52 -0800

    got file tree rendering for open project, no styling yet

--- renderer/components/FileTree.jsx
+++ renderer/components/FileTree.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import File from './File.jsx';
+import Directory from './Directory.jsx';
 const fs = require('fs');
 const path = require('path');
 const {remote, ipcRenderer, dialog} = require('electron');
 const fileTree = require('../../lib/file-tree-sync');
 
+
 export default class FileTree extends React.Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      fileTree: null
+    }
     this.fileTreeInit();
   }
 
   fileTreeInit() {
     ipcRenderer.on('openDir', (event, dirPathArr) => {
       this.setFileTree(dirPathArr);
-      let watch = fs.watch(dirPathArr, {recursive: true}, (eventType, fileName) => {
+      let watch = fs.watch(dirPathArr, { recursive: true }, (eventType, fileName) => {
         this.setFileTree(dirPathArr);
       });
     });
@@ -21,13 +27,25 @@ export default class FileTree extends React.Component {
 
   setFileTree(path) {
     const projFileTree = fileTree(path);
+    this.setState({
+      fileTree: projFileTree
+    })
   }
 
   render() {
-    return (
-      <div>
-        <h1>File Tree</h1>
-      </div>
-    )
+    if (this.state.fileTree) {
+      return (
+        <div>
+          <h1>File Tree</h1>
+          <Directory directory={this.state.fileTree} />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>File Tree</h1>
+        </div>
+      )
+    }
   }
 }
