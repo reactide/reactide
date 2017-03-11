b8d2b895c4b44ad6e7d07d2566c7725632b6c67e
Mark Marcelo <markmarcelo@Marks-MacBook-Pro.local>
2017-03-11 14:07:21 -0800

    css injected, refactored file tree

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 var path = require('path');
 
 module.exports = {
-    entry: './renderer/index.js',
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: 'webpack-bundle.js',
-        publicPath: ''
-    },
-	target:'electron',
-    module: {
-		loaders: [
-			{
-				test: /\.js$|\.jsx$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-						presets: ['es2015', 'react']
-				}
-			},
-			{
-				test: /(\.css|\.scss)$/,
-				loaders: ['style', 'css', 'sass']
-			}
-		]
-	}
+  entry: './renderer/index.js',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'webpack-bundle.js',
+    publicPath: '',
+  },
+  target: 'electron',
+  module: {
+    loaders: [
+      {
+        test: /\.js$|\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      }, {
+        test: /(\.css|\.scss)$/,
+        loaders: ['style', 'css', 'sass'],
+      }, {
+        test: /\.(eot|woff)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
 }
