0a991e18aae965604c2201a6a3d487c8dc5b094e
Jin Choi <jinchoi@ucdavis.edu>
2017-03-14 20:24:56 -0700

    kdjfs

--- renderer/components/FileTree.jsx
+++ renderer/components/FileTree.jsx
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const {remote, ipcRenderer, dialog} = require('electron');
 const fileTree = require('../../lib/file-tree-sync');
+// const fileTree = require('../../lib/file-tree');
 
 
 export default class FileTree extends React.Component {
