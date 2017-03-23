63d2c39e93b160afffdbd1d21322b6ef9265182a
Admin <admin@Admins-MacBook-Pro.local>
2017-03-22 22:45:00 -0700

    modularized main process, added delete functionality, prompt needs some serious styling

--- main/main.js
+++ main/main.js
@@ -3,8 +3,8 @@ const {BrowserWindow, ipcMain, Menu, app, dialog} = require('electron');
 const url = require('url');
 const path = require('path');
 const template = require('./menus/file');
-const {spawn, exec} = require('child_process');
-const localShortcut = require('electron-localshortcut');
+const registerShortcuts = require('./localShortcuts');
+const registerIpcListeners = require('./ipcMainListeners');
 const fs = require('fs');
 
 app.on('ready', () => {
@@ -15,44 +15,10 @@ app.on('ready', () => {
   win.loadURL('file://' + path.join(__dirname, '../renderer/index.html'));
   let menu = Menu.buildFromTemplate(template);
   Menu.setApplicationMenu(menu);
-  localShortcut.register(win, 'CommandOrControl+Alt+I', () => {
-    win.toggleDevTools();
-  });
-  localShortcut.register(win, 'CommandOrControl+S', () => {
-    win.webContents.send('saveFile');
-  })
-});
-function simulator(root) {
-  let spawn = exec('webpack-dev-server --inline --content-base ./public', {
-    cwd: path.join(__dirname, '../lib/temp/new-project')
-  }, (err, stdout, stderr) => {
-    let child = new BrowserWindow({
-      width: 800,
-      height: 600
-    });
-    child.loadURL('file://' + path.join(__dirname, '../lib/temp/new-project/client/public/index.html'));
-    child.toggleDevTools();
-    //set up hot loading or live reloading
-    //use fs.watch to have our own version of live reload?
-    //just fs.watch and on emit, do exec again
-    //and loadURl?
-  })
-}
 
-ipcMain.on('openSimulator', (event, root) => {
-  simulator(root);
-})
-ipcMain.on('createItem', (event, dirPath, name, type) => {
-  console.log('hello???')
-  if (type === 'file') {
-    fs.writeFile(path.join(dirPath, name), '', (err) => {
-      if (err) console.log(err);
-    })
-  } else {
-    fs.mkdir(path.join(dirPath, name), (err) => {
-      if (err) console.log(err);
-    })
-  }
-})
+  registerShortcuts(win);
+});
+registerIpcListeners();
+
 
 
