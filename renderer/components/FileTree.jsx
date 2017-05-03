9dd03a2a9e1f409f3266bba8c8afe8b45894f8eb
Jin Choi <jinchoi@ucdavis.edu>
2017-05-02 23:43:48 -0700

    refactored a bit in App.jsx, also added comments and made it a bit more readable

--- renderer/components/FileTree.jsx
+++ renderer/components/FileTree.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Directory from './Directory';
 import PropTypes from 'prop-types';
-import File from './File.jsx';
-const fs = require('fs');
-const path = require('path');
-const {remote, ipcRenderer, dialog} = require('electron');
 
 const FileTree = ({
   fileTree,
