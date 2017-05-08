54ceebf4a1b81d667eac2f3510d7bfd07326e64e
Jin Choi <jinchoi@ucdavis.edu>
2017-05-08 11:48:40 -0700

    changed openFile name to dblClickHandler, started proptype checking for components

--- renderer/components/CreateForm.js
+++ renderer/components/CreateForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const CreateForm = ({createItem}) => {
   return (
@@ -7,4 +8,9 @@ const CreateForm = ({createItem}) => {
     </div>
   )
 }
+
+CreateForm.propTypes = {
+  createItem: PropTypes.func.isRequired
+}
+
 export default CreateForm;
