1eba295534965ea00982b813a9db4fd55482e8ce
Mark Marcelo <mark@markmarcelo.com>
2017-05-30 12:30:22 -0700

    removed unused modules from main.js

--- main/main.js
+++ main/main.js
@@ -1,23 +1,17 @@
-'use strict'
+'use strict';
 
-const electron = require('electron');
-const { BrowserWindow, ipcMain, Menu, app, dialog } = require('electron');
-const url = require('url');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
-const template = require('./menus/mainMenu');
+const menuTemplate = require('./menus/mainMenu');
 const registerShortcuts = require('./localShortcuts');
 const registerIpcListeners = require('./ipcMainListeners');
-const fs = require('fs');
+const devtron = require('devtron');
 require('electron-debug')();
 
-const isDevelopment = (process.env.NODE_ENV === 'development');
-
 const installExtensions = async () => {
+  devtron.install();
   const installer = require('electron-devtools-installer');
-  const extensions = [
-    'REACT_DEVELOPER_TOOLS',
-    'REDUX_DEVTOOLS'
-  ];
+  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
   for (const name of extensions) {
     try {
@@ -28,32 +22,45 @@ const installExtensions = async () => {
   }
 };
 
-//Main window Init
+// Main window init
+// define window in global scope to prevent garbage collection
+let win = null;
+
 app.on('ready', async () => {
-
-  //install React & Redux Extensions
-  await installExtensions();
-
-  //initialize main window
-  let win = new BrowserWindow({
+  // initialize main window
+  win = new BrowserWindow({
     width: 1000,
-    height: 800
+    height: 800,
+    minWidth: 604,
+    minHeight: 283,
+    title: 'Reactide',
+    // titleBarStyle: hidden-inset, // pending
+    // icon: '', // pending
+    show: false
   });
-  
-  //load index.html to main window
+
+  // load index.html to main window
   win.loadURL('file://' + path.join(__dirname, '../renderer/index.html'));
 
-  //initialize menus
-  let menu = Menu.buildFromTemplate(template);
+  // initialize menus
+  const menu = Menu.buildFromTemplate(menuTemplate(win));
   Menu.setApplicationMenu(menu);
 
-  //toggle devtools only if development
-  if (isDevelopment) win.toggleDevTools();
+  // toggle devtools only if development
+  if (process.env.NODE_ENV === 'development') {
+    await installExtensions();
+    win.toggleDevTools();
+  }
 
-  //put Main window instance in global variable for use in other modules
+  // put Main window instance in global variable for use in other modules
   global.mainWindow = win;
 
-  //Register listeners and shortcuts
+  // Register listeners and shortcuts
   registerIpcListeners();
   registerShortcuts(win);
+
+  // Wait for window to be ready before showing to avoid white loading screen
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 });
