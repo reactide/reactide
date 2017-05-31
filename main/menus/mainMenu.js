c9a0b4092585cdbe412bbfcdc8d372efdbac0b22
Mark Marcelo <markmarcelo@Marks-MBP.T-mobile.com>
2017-05-31 00:20:59 -0700

    main menu updates and shortcuts

--- main/menus/mainMenu.js
+++ main/menus/mainMenu.js
@@ -1,59 +1,91 @@
-"use strict";
+'use strict';
 
-const { dialog } = require("electron");
-const path = require("path");
-const copy = require("../../lib/copy-directory");
-const deleteDirectory = require("../../lib/delete-directory");
+const { dialog } = require('electron');
+const path = require('path');
+const copy = require('../../lib/copy-directory');
+const deleteDirectory = require('../../lib/delete-directory');
 
 const menuTemplate = windowObj => [
   {
-    label: "Main"
+    label: 'Main',
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
   },
   {
-    label: "Test",
+    label: 'File',
     submenu: [
       {
-        label: "Open",
-        click: () => {
-          global.newProj = false;
-          const rootDir = dialog.showOpenDialog(windowObj, {
-            properties: ["openDirectory"]
-          });
-          if (rootDir) {
-            global.mainWindow.webContents.send("openDir", rootDir[0]);
-          }
-        }
-      },
-      {
-        label: "Save",
-        click: () => {
-          const save = dialog.showSaveDialog();
-          if (save) {
-            copy("./lib/temp/new-project", save[0]);
-          }
-          global.mainWindow.webContents.send("openDir", save[0]);
-        }
-      },
-      {
-        label: "New Project",
+        label: 'New Project',
         click: () => {
           // warn user of unsaved changes before belo
           global.newProj = true;
-          global.mainWindow.webContents.send("newProject");
-          deleteDirectory("./lib/new-project");
-          copy("./lib/new-project-template/new-project", "./lib/");
+          global.mainWindow.webContents.send('newProject');
+          deleteDirectory('./lib/new-project');
+          copy('./lib/new-project-template/new-project', './lib/');
           global.mainWindow.webContents.send(
-            "openDir",
-            path.join(__dirname, "../../lib/new-project")
+            'openDir',
+            path.join(__dirname, '../../lib/new-project')
           );
-        }
-      }
-    ]
+        },
+        accelerator: 'CommandOrControl+N',
+      },
+      {
+        label: 'Open…',
+        click: () => {
+          global.newProj = false;
+          const rootDir = dialog.showOpenDialog(windowObj, {
+            properties: ['openDirectory']
+          });
+          if (rootDir) {
+            global.mainWindow.webContents.send('openDir', rootDir[0]);
+          }
+        },
+        accelerator: 'CommandOrControl+O',
+      },
+      {
+        label: 'Save',
+        click: () => {
+          const save = dialog.showSaveDialog();
+          if (save) {
+            copy('./lib/temp/new-project', save[0]);
+          }
+          global.mainWindow.webContents.send('openDir', save[0]);
+        },
+        accelerator: 'CommandOrControl+S',
+      },
+    ],
   },
   {
-    label: "TEST2",
-    submenu: [{ role: "redo" }]
-  }
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+    ],
+  },
+  {
+    label: 'Selection',
+    submenu: [
+      { role: 'selectall' },
+    ],
+  },
+  {
+    role: 'window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'close' },
+    ],
+  },
 ];
 
 module.exports = menuTemplate;
