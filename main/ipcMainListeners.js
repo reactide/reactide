f18f2254c64e511540772132397925a1ac104710
Mark Marcelo <markmarcelo@Marks-MBP.T-mobile.com>
2017-05-31 13:09:17 -0700

    prettier formatting

--- main/ipcMainListeners.js
+++ main/ipcMainListeners.js
@@ -1,35 +1,34 @@
-'use strict'
+"use strict";
 
-const {ipcMain} = require('electron');
-const fs = require('fs');
-const path = require('path');
-const deleteItem = require('../lib/delete-directory');
-const simulator = require('./simulator')
+const { ipcMain } = require("electron");
+const fs = require("fs");
+const path = require("path");
+const deleteItem = require("../lib/delete-directory");
+const simulator = require("./simulator");
 
 module.exports = () => {
-
   //ipcMain listeners
-  ipcMain.on('openSimulator', (event, root) => {
+  ipcMain.on("openSimulator", (event, root) => {
     simulator(root);
   });
 
-  ipcMain.on('createItem', (event, dirPath, name, type) => {
-    if (type === 'file') {
-      fs.writeFile(path.join(dirPath, name), '', (err) => {
+  ipcMain.on("createItem", (event, dirPath, name, type) => {
+    if (type === "file") {
+      fs.writeFile(path.join(dirPath, name), "", err => {
         if (err) console.log(err);
-      })
+      });
     } else {
-      fs.mkdir(path.join(dirPath, name), (err) => {
+      fs.mkdir(path.join(dirPath, name), err => {
         if (err) console.log(err);
-      })
+      });
     }
   });
 
-  ipcMain.on('delete', (event, itemPath) => {
+  ipcMain.on("delete", (event, itemPath) => {
     deleteItem(itemPath);
   });
 
-  ipcMain.on('rename', (event, itemPath, newName) => {
+  ipcMain.on("rename", (event, itemPath, newName) => {
     fs.rename(itemPath, path.join(path.dirname(itemPath), newName));
   });
-}
+};
