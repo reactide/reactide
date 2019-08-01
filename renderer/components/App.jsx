06781a0b1bd68bba8931998b2412dcdd1d547282
Khalid Umar <khalid.umar101@gmail.com>
2019-08-01 15:51:37 -0400

    remove extra space on right side when toggling file tree

--- renderer/components/App.jsx
+++ renderer/components/App.jsx
@@ -688,7 +688,7 @@ export default class App extends React.Component {
 
   renderMainLayout() {
     return (
-      <ride-pane style={{ flexGrow: 0, flexBasis: '1150px'}}>
+      <ride-pane style={{ flexGrow: 1, flexBasis: '1150px'}}>
         {this.state.rootDirPath &&
           <React.Fragment>
             {this.renderMainTopPanel()}
