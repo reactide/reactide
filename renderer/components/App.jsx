f3ffc140276c8f3ed14367ede690b82f5137ed8d
juanhart1 <juanhart1@gmail.com>
2019-08-06 15:50:18 -0400

    Last Done: Pulled down from dev branch to get integrated npm package and close simulator fix Next Up: Begin working on styling features of file tree dropdown

--- renderer/components/App.jsx
+++ renderer/components/App.jsx
@@ -15,6 +15,7 @@ const { getTree, getFileExt } = require('../../lib/file-tree');
 const fs = require('fs');
 const path = require('path');
 const { File, Directory } = require('../../lib/item-schema');
+const { exec } = require('child_process');
 
 const importPathFunctions = require('../../importPath');
 
@@ -286,6 +287,11 @@ export default class App extends React.Component {
    * calls file-tree module and sets state with file tree object representation in callback
    */
   setFileTree(dirPath) {
+    if (!fs.existsSync(path.join(dirPath, '/reactide.js'))) {
+      exec(`npm i -S npmtest-reactide && echo 'const yes = require("'npmtest-reactide'") \n yes.config()' >> reactide.js && node reactide.js `,{
+        cwd: dirPath
+      });  
+    }
     getTree(dirPath, fileTree => {
       //if watcher instance already exists close it as it's for the previously opened project
       if (this.state.watch) {
@@ -322,6 +328,7 @@ export default class App extends React.Component {
               parentDir.subdirectories.push(new Directory(absPath, name));
             } else {
               parentDir.files.push(new File(absPath, name, getFileExt));
+              console.log(parentDir.files);
             }
           } else if (this.state.fileChangeType === 'rename' && this.state.newName) {
             //rename handler
@@ -542,8 +549,12 @@ export default class App extends React.Component {
    * Changes state of simulator to true to trigger conditional rendering of Editor and Simulator
    */
   openSimulatorInMain() {
-    this.setState({ simulator: true });
-    ipcRenderer.send('start simulator', 'helloworld');
+    if(this.state.simulator === false){
+      this.setState({ simulator: true });
+      ipcRenderer.send('start simulator', 'helloworld');
+    } else {
+      this.closeSim()
+    }
   }
   /**
    * closes any open dialogs, handles clicks on anywhere besides the active open menu/form
@@ -675,7 +686,7 @@ export default class App extends React.Component {
         <React.Fragment>
           <InWindowSimulator url={this.state.url} />
           <button className="btn" onClick={this.closeSim}>
-            Close Simulator
+            {/* Close Simulator */}
           </button>
         </React.Fragment>
       );
