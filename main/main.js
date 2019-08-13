1e3ed83091d5192c8b3d274f497fe57fcfe0a09a
Khalid Umar <khalid.umar101@gmail.com>
2019-08-13 14:59:19 -0400

    add auto update

--- main/main.js
+++ main/main.js
@@ -11,11 +11,13 @@ const devtron = require('devtron');
 const { exec } = require('child_process');
 require('electron-debug')();
 
+
 require('update-electron-app')({
   repo: 'khalid050/reactide',
   updateInterval: '5 minutes',
   logger: require('electron-log')
 })
+
 
 const projInfoPath = path.join(__dirname, '../lib/projInfo.js');
 const projInfo = {
